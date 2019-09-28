f79a9569c80ea9d82c0eb8faf36c8923c73c7cab
Arthur Damm <indigoarthur@gmail.com>
2019-09-28 18:29:29 +0000

    Add task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,85 @@
+const listAmenities = {};
+
+$(function () {
+  $('div.amenities li input').change(
+    function () {
+      if ($(this).is(':checked')) {
+        listAmenities[($(this).attr('data-id'))] = $(this).attr('data-name');
+        $('div.amenities h4').text(Object.values(listAmenities).join(', '));
+        console.log('checked', $(this).attr('data-name'));
+      } else {
+        delete listAmenities[($(this).attr('data-id'))];
+        $('div.amenities h4').text(Object.values(listAmenities).join(', '));
+        console.log('un-checked', $(this).attr('data-name'));
+      }
+    });
+
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  $.ajax('http://0.0.0.0:5001/api/v1/places_search', {
+    data: JSON.stringify({}),
+    contentType: 'application/json',
+    type: 'POST',
+    success: data => {
+      for (const place of data) {
+        const template = `<article>
+
+      <div class="title">
+
+        <h2>${place.name}</h2>
+
+        <div class="price_by_night">
+
+    $${place.price_by_night}
+
+        </div>
+      </div>
+      <div class="information">
+        <div class="max_guest">
+    <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+
+    <br />
+
+    ${place.max_guest} Guests
+
+        </div>
+        <div class="number_rooms">
+    <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+
+    <br />
+
+    ${place.number_rooms} Bedrooms
+        </div>
+        <div class="number_bathrooms">
+    <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+
+    <br />
+
+    ${place.number_bathrooms} Bathroom
+
+        </div>
+      </div>
+
+      <!-- **********************
+     USER
+     **********************  -->
+
+      <div class="description">
+
+        ${place.description}
+
+      </div>
+
+    </article> <!-- End 1 PLACE Article -->`;
+        $('section.places').append(template);
+        console.log('T:', template);
+      }
+    }
+  });
+});
